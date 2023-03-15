83a1ea02e454b322ae270d386b8a54499bf5299d
Logan Walsh <lwalsh6@ut.utm.edu>
2023-03-14 19:24:29 -0500

    Very basic version of the video player, bugs listed in readme

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +12,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Threading; //Added for the timer
 using System.Windows.Shapes;
+
+//Video player Program by Logan Walsh and Zachary Rose
+//Last Edited: 3-14-2023
 
 namespace MediaPlayer
 {
@@ -20,9 +25,62 @@ namespace MediaPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        //This is the timer used for running the video
+        DispatcherTimer vidTimer;
         public MainWindow()
         {
             InitializeComponent();
+            vidTimer = new DispatcherTimer();
+            vidTimer.Interval = TimeSpan.FromMilliseconds(500);
+            vidTimer.Tick += new EventHandler(tickTimer);
+
+        }
+
+        void tickTimer(object sender, EventArgs e)
+        {
+            Seeker.Value = viewport.Position.TotalSeconds;
+        }
+        //This probably won't remain a global variable
+        bool isPlaying;
+        private void PlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying == true)
+            {
+                viewport.Pause();
+                isPlaying = false;
+            }
+            else if (isPlaying == false)
+            {
+                viewport.Play();
+                isPlaying = true;
+            }
+
+        }
+
+        private void DropDown(object sender, DragEventArgs e)
+        {
+            string vidFile = (string)((DataObject)e.Data).GetFileDropList()[0];
+            //Sets the dropped file to the viewport
+            viewport.Source = new Uri(vidFile);
+
+            viewport.LoadedBehavior = MediaState.Manual;
+            viewport.UnloadedBehavior = MediaState.Manual;
+            viewport.Volume = 1; //Temporary, will use a slider
+            viewport.Play();
+            isPlaying = true;
+        }
+
+        private void OpenMedia(object sender, RoutedEventArgs e)
+        {
+            TimeSpan mediaRunTime = viewport.NaturalDuration.TimeSpan;
+            Seeker.Maximum = mediaRunTime.TotalSeconds;
+            //Starts a timer needed to run the video
+            vidTimer.Start();
+        }
+
+        private void Seeker_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            viewport.Position = TimeSpan.FromSeconds(Seeker.Value);
         }
     }
 }
