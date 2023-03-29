e35eabc3b70b46270c8992c66aabd4ee112273a1
Zachary Rose <zacerose@ut.utm.edu>
2023-03-29 13:45:25 -0500

    Added file choosing dialog under the menu, smoothed the seeker bar, and started a rudimentary reverse playback feature.

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -27,11 +27,12 @@ namespace MediaPlayer
     {
         //This is the timer used for running the video
         DispatcherTimer vidTimer;
+        bool playing_fowards = true;
         public MainWindow()
         {
             InitializeComponent();
             vidTimer = new DispatcherTimer();
-            vidTimer.Interval = TimeSpan.FromMilliseconds(500);
+            vidTimer.Interval = TimeSpan.FromMilliseconds(42);
             vidTimer.Tick += new EventHandler(tickTimer);
             viewport.LoadedBehavior = MediaState.Manual;
             viewport.UnloadedBehavior = MediaState.Manual;
@@ -39,7 +40,13 @@ namespace MediaPlayer
 
         void tickTimer(object sender, EventArgs e)
         {
-            Seeker.Value = viewport.Position.TotalSeconds;
+            Seeker.Value = viewport.Position.TotalMilliseconds;
+
+            if (!playing_fowards && isPlaying) {
+                Seeker.Value -= 42;
+                //double pos = viewport.Position.TotalMilliseconds;
+                //viewport.Position -= TimeSpan.FromMilliseconds(42);
+            }   
         }
         //This probably won't remain a global variable
         bool isPlaying;
@@ -72,14 +79,16 @@ namespace MediaPlayer
         private void OpenMedia(object sender, RoutedEventArgs e)
         {
             TimeSpan mediaRunTime = viewport.NaturalDuration.TimeSpan;
-            Seeker.Maximum = mediaRunTime.TotalSeconds;
+            // Represents each millisecond of the video opened
+            Seeker.Minimum = 0;
+            Seeker.Maximum = mediaRunTime.TotalMilliseconds;
             //Starts a timer needed to run the video
             vidTimer.Start();
         }
 
         private void Seeker_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            viewport.Position = TimeSpan.FromSeconds(Seeker.Value);
+            viewport.Position = TimeSpan.FromMilliseconds(Seeker.Value);
         }
 
         private void Playback_TextChanged(object sender, TextChangedEventArgs e)
@@ -91,8 +100,9 @@ namespace MediaPlayer
 
             if (parseTest == true)
             {
-                playback = double.Parse(ManualPlayback.Text);
-                viewport.SpeedRatio = playback;
+                // only change the playback speed if between these values
+                if (playback > 0 && playback < 3)
+                    viewport.SpeedRatio = playback;
             }
             else if (parseTest == false)
             {
@@ -105,6 +115,7 @@ namespace MediaPlayer
         //TEMPORARY, Want to use the Playback_TextChanged element for this feature
         private void Reverse_Click(object sender, RoutedEventArgs e)
         {
+            playing_fowards = !playing_fowards;          
         }
 
         private void FREV_Click(object sender, RoutedEventArgs e)
@@ -131,7 +142,20 @@ namespace MediaPlayer
 
         private void LoadFile_Click(object sender, RoutedEventArgs e)
         {
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+            fileDialog.DefaultExt = ".mp4";
+            fileDialog.Filter = "MP4 Files (*.mp4)|*.mp4|WMV Files (*.wmv)|*.wmv|AVI Files (*.avi)|*.avi";
+            Nullable<bool> success = fileDialog.ShowDialog();
+            if (success == true)
+            {
+                string vidFile = fileDialog.FileName;
+                viewport.Source = new Uri(vidFile);
 
+                viewport.Volume = 1; //Temporary, will use a slider
+                viewport.Play();
+                isPlaying = true;
+
+            }          
         }
     }
 }
